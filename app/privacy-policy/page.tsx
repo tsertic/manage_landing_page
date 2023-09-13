506f8bfd9dca5d9e6c096a06154bfdbd6d28b27d
tsertic <tsertic5@gmail.com>
2023-09-13 07:46:36 +0200

    added placeholders for missing pages

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      Not in scope of this project, just for testing link and paths
+    </div>
+  );
+};
+
+export default page;
